fa6fce8393c6b91ba8ec02899c5dfd2961a11ae2
ablades <audaris@gmail.com>
2020-07-17 15:52:49 -0500

    create home and title components

---
+++ src/components/home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Title from './title'
+
+function Home(){
+    <div className="home">
+        <Title title="FEW 2.3 Final - Audi Blades" /> 
+    </div>
+}
+
+export default Home
